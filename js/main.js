1791c63cbf63d8fdfb1c0a9d45e5c7c6947c80c3
otabekdev1 <forthepubghero@gmail.com>
2022-12-02 15:59:12 +0500

    clock

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function displayTime() {
+  let datetime = new Date();
+  let hrs = datetime.getHours();
+  let min = datetime.getMinutes();
+  let sec = datetime.getSeconds();
+  var session = document.getElementById("session");
+  if (hrs >= 12) {
+    session.innerHTML = "PM";
+  } else {
+    session.innerHTML = "AM";
+  }
+  if (hrs > 12) {
+    hrs = hrs - 12;
+  }
+  document.getElementById("hours").innerHTML = hrs;
+  document.getElementById("minutes").innerHTML = min;
+  document.getElementById("seconds").innerHTML = sec;
+}
+setInterval(displayTime, 10);
